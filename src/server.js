ede86e3d80c852ba7e13fd76e419208e22b843ce
FusProgramming <34615176+FusProgramming@users.noreply.github.com>
2019-12-04 04:25:01 -0500

    D

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const app = Express();
 
 const uri = 'mongodb+srv://RobFus:H2runo01-bass01@cluster0-b9vqw.mongodb.net/test?retryWrites=true&w=majority';
 // Connect to the 'test' database. Ensure you have started the mongod process!
-Mongoose.connect(uri , {useNewUrlParser: true});
+Mongoose.connect(uri , {useNewUrlParser: true, useCreateIndex: true});
 // When Mongoose has connected to your MongoDB, print out a log message
 Mongoose.connection.once('open', () => console.log("Connected to database!"));
 //----------------------------------------------------------------------------------------------------------------------
