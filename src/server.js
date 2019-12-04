27c87c0e19b5e14acfb55a6f546b8772d5cda654
FusProgramming <34615176+FusProgramming@users.noreply.github.com>
2019-12-04 04:22:59 -0500

    D

--- src/server.js
+++ src/server.js
@@ -12,7 +12,50 @@ const uri = 'mongodb+srv://RobFus:H2runo01-bass01@cluster0-b9vqw.mongodb.net/tes
 Mongoose.connect(uri , {useNewUrlParser: true});
 // When Mongoose has connected to your MongoDB, print out a log message
 Mongoose.connection.once('open', () => console.log("Connected to database!"));
+//----------------------------------------------------------------------------------------------------------------------
+app.get('/api/beer', async (request, response) => {
+    console.log('A GET request came in asking for all users');
+    const beers = await Beer.find({});
+    return response.send(beers).status(200);
+});
 
+// When a POST request comes in on this route, create a new user and return success with a 200 code or failure with a 400 code
+app.post('/api/beers', async (request, response) => {
+
+    console.log('A request came in with the body: ' + JSON.stringify(request.body));
+    const { storeName, beerName, beerType, address, city, state} = request.body;
+    try {
+        await Beer.create({
+            storeName: storeName,
+            beerName: beerName,
+            beerType: beerType,
+            address: address,
+            city: city,
+            state: state
+        });
+
+        console.log(`A new user was created with name: '${storeName}' and email address: '${beerName}'`);
+        return response.sendStatus(200);
+
+    } catch (error) {
+        console.error('Something went wrong while creating a new user: ' + error.message);
+        return response.sendStatus(400);
+    }
+});
+
+app.delete('/api/beer/', async (request, response) => {
+    const { beerId, storeName, beerName, beerType, address, city, state} = request.body;
+    try {
+
+        console.log('Delete Request');
+        const beers = await Beer.deleteOne({});
+
+        return response.send(beers).status(200);
+    } catch (error) {
+        console.error('Something went wrong while trying to delete: ' + error.message);
+        return response.sendStatus(400);
+    }
+});
 // We will use an Express Middleware called json. Middleware is called before any REST route gets your request.
 // Middleware can reject requests prior to reaching your REST routes or append data to your route.
 // The Express.json middleware provides easy to use JSON on POST requests.
@@ -77,50 +120,7 @@ app.post('/api/users', async (request, response) => {
     }
 });
 
-//----------------------------------------------------------------------------------------------------------------------
-app.get('/api/beer', async (request, response) => {
-    console.log('A GET request came in asking for all users');
-    const beers = await Beer.find({});
-    return response.send(beers).status(200);
-});
 
-// When a POST request comes in on this route, create a new user and return success with a 200 code or failure with a 400 code
-app.post('/api/beers', async (request, response) => {
-
-    console.log('A request came in with the body: ' + JSON.stringify(request.body));
-    const { storeName, beerName, beerType, address, city, state} = request.body;
-    try {
-        await Beer.create({
-            storeName: storeName,
-            beerName: beerName,
-            beerType: beerType,
-            address: address,
-            city: city,
-            state: state
-        });
-
-        console.log(`A new user was created with name: '${storeName}' and email address: '${beerName}'`);
-        return response.sendStatus(200);
-
-    } catch (error) {
-        console.error('Something went wrong while creating a new user: ' + error.message);
-        return response.sendStatus(400);
-    }
-});
-
-app.delete('/api/beer/', async (request, response) => {
-    const { beerId, storeName, beerName, beerType, address, city, state} = request.body;
-    try {
-
-        console.log('Delete Request');
-        const beers = await Beer.deleteOne({});
-
-        return response.send(beers).status(200);
-    } catch (error) {
-        console.error('Something went wrong while trying to delete: ' + error.message);
-        return response.sendStatus(400);
-    }
-});
 
 const port = process.env.PORT || 4300;
 app.listen(port, () => console.log(`Server has started on localhost:${port}`));
