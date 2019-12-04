1878f0a4d5ccdff976e95e28ed5be03220c5e39c
FusProgramming <34615176+FusProgramming@users.noreply.github.com>
2019-12-04 00:59:37 -0500

    D

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const Beer = require('./models/beer');
 const Post = require('./models/registerpost');
 const app = Express();
 
-const uri = "mongodb://localhost:27017/lol";
+const uri = 'mongodb://localhost:27017/lol';
 // Connect to the 'test' database. Ensure you have started the mongod process!
 Mongoose.connect(uri , {useNewUrlParser: true});
 // When Mongoose has connected to your MongoDB, print out a log message
