0e70c2ca64b1f59147285a4fd91ce0477a6763da
FusProgramming <34615176+FusProgramming@users.noreply.github.com>
2019-12-05 00:41:03 -0500

    D

--- src/server.js
+++ src/server.js
@@ -1,124 +1,135 @@
-const Express = require('express');
-const Mongoose = require('mongoose');
-const path = require('path');
-const bcrypt = require('bcryptjs');
-const User = require('./models/user');
-const Beer = require('./models/beer');
+const Express = require("express");
+const Mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const path = require("path");
+const User = require("./models/user");
 
 const app = Express();
-
-const uri = 'mongodb+srv://RobFus:H2runo01-bass01@cluster0-b9vqw.mongodb.net/test?retryWrites=true&w=majority';
 // Connect to the 'test' database. Ensure you have started the mongod process!
-Mongoose.connect(uri , {useNewUrlParser: true, useCreateIndex: true});
+const clientAppDirectory = path.join(__dirname, "../public", "build");
+const uri =
+    "mongodb+srv://webdev:paulmorton@cluster0-tibnr.mongodb.net/paul?retryWrites=true&w=majority";
+
+Mongoose.connect(uri, { useNewUrlParser: true });
+
 // When Mongoose has connected to your MongoDB, print out a log message
-Mongoose.connection.once('open', () => console.log("Connected to database!"));
+Mongoose.connection.once("open", () => console.log("Connected to database!"));
+
 // We will use an Express Middleware called json. Middleware is called before any REST route gets your request.
 // Middleware can reject requests prior to reaching your REST routes or append data to your route.
 // The Express.json middleware provides easy to use JSON on POST requests.
-// Specify where the compiled React app lives (copied the files manually from the client build)
-const clientAppDirectory = path.join(__dirname, '../public', 'build');
-
 app.use(Express.json());
 app.use(Express.static(clientAppDirectory));
-// When a GET request comes in on this route, find all users in the database and return them with a 200 code
-app.post('/api/postly', (request, response) => {
-    console.log('Received request: ' + JSON.stringify(request.body));
-    const { testData } = request.body;
-    if (testData === 'teapot') {
-        return response.sendStatus(418);
-    }
-    // Simply return whatever the client sent to show that the server received it
-    return response.status(200);
-});
 
-// Any other GET request that doesn't match previous routes should return the website
-app.get('/*', (request, response) => {
-    const indexPath = path.join(clientAppDirectory, 'index.html');
+app.get("/*", (request, response) => {
+    const indexPath = path.join(clientAppDirectory, "index.html");
+
     return response.sendFile(indexPath);
 });
-app.get('/api/user', async (request, response) => {
 
-    console.log('A GET request came in asking for all users');
+app.get("/api/users", async (request, response) => {
+    console.log("A GET request came in asking for all users");
 
     const users = await User.find({});
 
     return response.send(users).status(200);
 });
 
-// When a POST request comes in on this route, create a new user and return success with a 200 code or failure with a 400 code
-app.post('/api/users', async (request, response) => {
-    console.log('A request came in with the body: ' + JSON.stringify(request.body));
-    const { userName, firstName, lastName, emailAddress, password} = request.body;
+app.post("/api/users", async (request, response) => {
+    // Pull the email and password out of the request body
+
+    console.log("A Post request came in for users");
+    const { email, password } = request.body;
+
     try {
-        const existingUser = await User.findOne({ emailAddress: { $eq: emailAddress}});
-        if(existingUser) {
-            console.log(`A user with the email address '${emailAddress}' already exists, rejecting request with a 400`);
+        // Check if a user with that email already exists in the database
+        const existingUser = await User.findOne({ email: { $eq: email } });
+
+        // There already is a user with that email, don't tell the user this to mitigate user enumeration, just return a 400
+        if (existingUser) {
+            console.log(
+                `A user with the email address '${email}' already exists, rejecting request with a 400`
+            );
+
             return response.sendStatus(400);
         }
 
+        // Hash the plaintext password with 12 salt rounds
         await bcrypt.hash(password, 12, async (error, hash) => {
+            // Something went wrong (not common), return a 400
             if (error) {
-                console.log('An error occured hashing the password: ' + error.message);
+                console.log("An error occured hashing the password: " + error.message);
+
                 return response.sendStatus(400);
             }
+
             // Create a new user with the password hash and email address
-            const user = await User.create({userName, firstName, lastName, emailAddress, password: hash});
-            console.log('Saved user email was ' + user.emailAddress + ' and password was ' + user.password);
+            const user = await User.create({email, passwordDigest: hash});
+
+            console.log(
+                "Saved user email was " +
+                user.email +
+                " and passwordDigest was " +
+                user.passwordDigest
+            );
+
             return response.sendStatus(200);
         });
-
-        console.log(`A new user was created with name: '${userName}' and email address: '${emailAddress}'`);
-        return response.sendStatus(200);
     } catch (error) {
-        console.error('Something went wrong while creating a new user: ' + error.message);
-        return response.sendStatus(400);
+        console.log("An unexpected error occured: " + error.message);
+
+        return response.sendStatus(500);
     }
 });
 
-//----------------------------------------------------------------------------------------------------------------------
-app.get('/api/beer', async (request, response) => {
-    console.log('A GET request came in asking for all users');
-    const beers = await Beer.find({});
-    return response.sendFile(beers).status(200);
-});
+// This endpoint will be used to log in
+app.post("/api/sessions", async (request, response) => {
+    // Pull the login credentials from the request
+    const { email, password } = request.body;
 
-// When a POST request comes in on this route, create a new user and return success with a 200 code or failure with a 400 code
-app.post('/api/beers', async (request, response) => {
+    // Find a user with the email address specified
+    const user = await User.findOne({ email: { $eq: email } });
 
-    console.log('A request came in with the body: ' + JSON.stringify(request.body));
-    const { storeName, beerName, beerType, address, city, state} = request.body;
-    try {
-        await Beer.create({
-            storeName: storeName,
-            beerName: beerName,
-            beerType: beerType,
-            address: address,
-            city: city,
-            state: state
-        });
+    // No user was found with that email address, dont tell the user this to mitigate user enumeration attacks, just return a 400
+    if (!user) {
+        console.log("No user was found with the email address: " + email);
 
-        console.log(`A new user was created with name: '${storeName}' and email address: '${beerName}'`);
-        return response.sendStatus(200);
-
-    } catch (error) {
-        console.error('Something went wrong while creating a new user: ' + error.message);
         return response.sendStatus(400);
     }
-});
 
-app.delete('/api/beer/', async (request, response) => {
-    const { beerId, storeName, beerName, beerType, address, city, state} = request.body;
-    try {
+    // Let Bcrypt figure out if the given plaintext password equals the one saved in the database
+    // Bcrypt will handle the random salt it gave the password when the user was created
+    bcrypt.compare(password, user.passwordDigest, (error, result) => {
+        // Not common, return a 400 if there was an error
+        if (error) {
+            console.error(
+                "There was an error checking the users password hash: " + error.message
+            );
 
-        console.log('Delete Request');
-        const beers = await Beer.deleteOne({});
+            return response.sendStatus(400);
+        }
 
-        return response.send(beers).status(200);
-    } catch (error) {
-        console.error('Something went wrong while trying to delete: ' + error.message);
+        // Don't perform truthy logic here, check strict against a boolean
+        if (result === true) {
+            console.log("User successfully logged in!");
+
+            return response.sendStatus(200);
+        }
+
+        console.log("User failed login, incorrect password");
+
         return response.sendStatus(400);
-    }
+    });
+});
+app.get("/*", (request, response) => {
+    const indexPath = path.join(clientAppDirectory, "index.html");
+
+    return response.sendFile(indexPath);
 });
 
-const port = process.env.PORT || 4300;
+const port = process.env.PORT;
+if (port == null || port == "") {
+    port = 8000;
+}
+app.listen(port);
 app.listen(port, () => console.log(`Server has started on localhost:${port}`));
